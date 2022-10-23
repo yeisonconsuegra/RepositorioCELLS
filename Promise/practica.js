8aa5a2f80e85032f063862998dd14329386614fe
yeisonconsuegra <81399945+yeisonconsuegra@users.noreply.github.com>
2022-10-23 17:36:28 -0500

    Update practica.js

--- Promise/practica.js
+++ Promise/practica.js
@@ -1,33 +1,3 @@
-/*
-const empleados = [{id: 1, nombre: 'julian'},{id: 2, nombre: 'Diana'},{id: 3, nombre: 'Sara'},{id: 4, nombre: 'Gabriela'}];
-const salarios = [{id: 1, salario: 10000},{id: 2, salario: 2000}];
-
-
-const getEmpleado = (id) => {
-    return new Promise((resolve, reject) => {
-        const empleado = empleados.find(e => e.id === id)?.nombre;
-        (empleado) ? resolve(empleado) : reject(`Empleado con id ${id} no existe`);
-    });
-}
-
-const getSalario = (id) => {
-    return new Promise((resolve, reject) => {
-        const salario = salarios.find(s => s.id === id)?.salario;
-        (salario) ? resolve(salario) : reject(`id ${id} no tiene salario`);
-    });
-}
-
-
-const getInfoEmpelado = async(id) => {
-    try{
-        const empleado = await getEmpleado(id);
-        const salario = await getSalario(id);
-        console.log(`El empleado ${empleado} tiene un salario de $${salario}`);
-    }catch(error){
-        console.log(error);
-    }
-}
-*/
 const estudiantes = [
     { id: 1, nombre: 'Ever' },
     { id: 2, nombre: 'Yeison' },
